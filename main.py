981c2bb3edcebdbc7d0fd1b1672168a74e466081
Siju302 <sijuadeoyelami10@gmail.com>
2025-04-20 11:50:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import math
+
 # Parent class
 class Polygon:
     def __init__(self, perimeter, apothem):
         self.perimeter = perimeter
         self.apothem = apothem
 
-    # Method to get area of polygon 
     def area(self):
         return self.apothem * self.perimeter / 2
 
@@ -15,12 +16,23 @@ class Polygon:
 class Square(Polygon):
     def __init__(self, side):
         self.side = side
-        perimeter = 4 * side  
-        apothem = side / 2    
+        perimeter = 4 * side
+        apothem = side / 2
         super().__init__(perimeter, apothem)
 
-# Object creation
+# Example usage
 obj = Square(4)
+obj.display()
 
-# Calling method display
+# Child class
+class Pentagon(Polygon):
+    def __init__(self, side):
+        self.side = side
+        perimeter = 5 * side
+        # cot(π/5) = 1 / tan(π/5)
+        apothem = (side / 2) / math.tan(math.pi / 5)
+        super().__init__(perimeter, apothem)
+
+# Example usage
+obj = Pentagon(5)
 obj.display()
